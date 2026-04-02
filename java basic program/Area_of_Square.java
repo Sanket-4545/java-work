d457c9de45eb398dbaaf2ee50e10bb4d8492fdba
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-02 18:34:24 +0530

    Add area of square

---
+++ java basic program/Area_of_Square.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Area_of_Square {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+System.out.println("Enter the value of the side of the squre :");
+float side = sc.nextFloat();
+float Area = side * side;
+System.out.println("The Area of the squre is :" +Area);
+
+  }  
+}
