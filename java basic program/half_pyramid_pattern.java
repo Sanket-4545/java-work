05dccec5d21a3e37bd4b87509a7c4afb1e13946b
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-03 15:28:20 +0530

    half_pyramid_pattern program

---
+++ java basic program/half_pyramid_pattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+ public class half_pyramid_pattern {
+ 
+    public static void main(String[] args) {
+        int n = 5;
+        for(int line = 1; line<=n; line ++){
+            for (int number = 1; number <=line ; number ++){
+                System.out.print(number);
+            }
+            System.out.println();
+
+        }
+    }
+ }
