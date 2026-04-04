505f994b257a4fb29e63fa3bcbe03b34dfe0183a
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-04 18:00:54 +0530

    java basic program to print star in pattern simple way called as only use print

---
+++ java basic program/javaBasic.java
@@ -0,0 +1,11 @@
+public class javaBasic {
+    public static void main(String[] args) {
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* * ");
+        System.out.println("*  ");
+        int a = 10;
+        a = 50;
+        System.out.print(a);
+    }
+}
