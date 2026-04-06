fc9c380ca380a87072757b3fb18bc732b476bfbb
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-06 18:54:08 +0530

    linearSearch

---
+++ Arrays/linearSearch.java
@@ -0,0 +1,21 @@
+public class linearSearch {
+    public static int LinearSearch(int numbers[], int key) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int numbers[] = {2, 4, 5, 7, 12, 15, 18, 23, 27, 30};
+        int key = 15;
+        int result = LinearSearch(numbers, key);
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
