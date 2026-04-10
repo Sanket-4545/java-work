36149f70c4c0ca1b14c58299bcac1d85b6284e41
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-10 20:50:46 +0530

    PatternSecond-advanced

---
+++ PatternSecond-advanced/halfPyramidNumber.java
@@ -0,0 +1,15 @@
+public class halfPyramidNumber {
+    public static void HalfPyramidNumber(int n) {
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= n+1-i; j++)
+            {
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+        
+    }
+   public static void main(String[] args) {
+        HalfPyramidNumber(5);
+   } 
+}
