1dda83ae5d8d84ed9625f095bb43ed402f96fdef
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-16 21:03:37 +0530

    Excer_Q1 Avg.of threeNumber

---
+++ Function OR Method/Excer_Q1.java
@@ -0,0 +1,10 @@
+
+public class Excer_Q1 {
+    public static void Avg_of_threeNumber(int a,int b,int c) {
+ int sum = a + b + c;
+        System.out.println("The sum of three number is :" + sum);
+    }
+public static void main(String[] args) {
+ Avg_of_threeNumber(45, 62, 58);   
+}
+}
