335e415cc1b33c771f58736ca346ee031fcbfc34
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-17 20:57:09 +0530

    Find ever or odd number Excer_Q2

---
+++ Function OR Method/Excer_Q2.java
@@ -0,0 +1,14 @@
+public class Excer_Q2 {
+    public static void isEven(int n) {
+        if (n % 2 == 0) {
+            System.out.println("Your number"+n+" is Even :");
+        }
+        else {
+              System.out.println("Your number " +n+" is Odd :" );
+        }
+
+    }
+    public static void main(String[] args) {
+        isEven(0);
+    }
+}
