d06ece659c3227385db8322e78163f4287c1f35a
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-25 20:54:48 +0530

    product

---
+++ Function OR Method/Product.java
@@ -0,0 +1,10 @@
+public class Product {
+    public static int Multiply(int a, int b){
+        int product = a*b;
+        return product;
+    }
+ public static void main(String[] args) {
+    int ans = Multiply(7,8);
+    System.out.println(ans);
+ }   
+}
