fed6798bcc57b0d81825ac0246f2d87a8db18f0b
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-27 08:01:56 +0530

    String_length

---
+++ String/String_length.java
@@ -0,0 +1,7 @@
+public class String_length {
+    public static void main(String[] args) {
+        String Name = "Sanket Bhojane";
+        System.out.println(Name.length());
+        // this is cound space between two words also and total count or lenght is 14
+    }
+}
