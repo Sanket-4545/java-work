0b074f3a276940fd5c553d6eff89731b44aad3f9
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-28 08:35:22 +0530

    LargestSting

---
+++ String/LargestSting.java
@@ -0,0 +1,15 @@
+public class LargestSting {
+    public static void main(String[] args) {
+        String names[] = { "Sanket" , "Dhanraj" , "Tejas"};
+        String largestStr = names [0];
+        for (int i = 0 ; i<names.length; i++){
+if(largestStr.compareTo(names[i])<0){
+    largestStr = names[i];
+    // largest mean = a>b>c>d.....>z
+
+}
+        }
+        System.out.println(largestStr);
+
+    }
+}
