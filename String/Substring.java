1815ed72fd3a08fe2af768e93c108dc0cc5a266e
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-28 07:40:06 +0530

    Substring

---
+++ String/Substring.java
@@ -0,0 +1,16 @@
+public class Substring {
+    public static String Substring(String str, int si, int ei) {
+        String subString = "";
+        for (int i = si ; i< ei ; i++){
+            subString += str.charAt(i);
+        }
+
+
+     return subString;   
+    }
+    public static void main(String[] args) {
+        String str = "Sanket Bhojane";
+        // int str let space between the two words that it read of count space also
+        System.out.println(Substring(str, 0, 9));
+    }
+}
