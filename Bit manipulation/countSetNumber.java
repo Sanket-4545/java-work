a3ef73cd8aa058661383ec78240e88d6a42a4021
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-04-30 18:47:50 +0530

    countSetNumber

---
+++ Bit manipulation/countSetNumber.java
@@ -0,0 +1,15 @@
+public class countSetNumber {
+    public static int countSetBit(int n){
+int count = 0;
+while (n>0) {
+    if ((n & 1) == 1) {
+        count++;
+    }
+    n = n >> 1;
+}
+return count;
+    }
+    public static void main(String[] args) {
+        System.out.println(countSetBit(7)); 
+    }
+}
