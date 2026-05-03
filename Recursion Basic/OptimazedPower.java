77fc8cb7dcc95552f6e4e687ce373626d89d3b51
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-05-03 18:38:09 +0530

    OptimazedPower

---
+++ Recursion Basic/OptimazedPower.java
@@ -0,0 +1,19 @@
+public class OptimazedPower {
+    public static int OptPower(int a, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int halfPower = OptPower(a, n/2) ;
+        int halfPowerSq = halfPower * halfPower ;
+
+        if ( n % 2 != 0) {
+            halfPowerSq = a * halfPowerSq ;
+        }
+        return halfPowerSq ;
+    }
+    public static void main(String[] args) {
+        int a = 2;
+        int n = 10;
+        System.out.println(OptPower(a, n));
+    }
+}
