3ed105634931f2b5853444321561c3db635fb341
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-06-25 22:35:56 +0530

    two pair sum

---
+++ ArrayList-23/PairSum1.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+
+public class PairSum1 {
+
+    public static boolean PairSum1(ArrayList<Integer> list, int target) {
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == target) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> height = new ArrayList<>();
+        height.add(1);
+        height.add(8);
+        height.add(6);
+        height.add(2);
+        height.add(5);
+        height.add(4);
+        height.add(8);
+        height.add(3);
+        height.add(7);
+
+        System.out.println(PairSum1(height, 10));
+    }
+}
