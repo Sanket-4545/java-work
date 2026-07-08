a6b35f257c9f8de4c3a5b6bb9e1cf64efe80d86a
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-07-08 14:26:48 +0530

    sum of the Node

---
+++ 29. Binary Trees (Part 1)/sumOfNode.java
@@ -0,0 +1,44 @@
+
+public class sumOfNode {
+
+    static class Node {
+    int data;
+    Node left, right;
+    
+     public Node(int data){
+        this.data = data;
+     this.left = null;
+     this.right = null;
+        
+    }
+    public static int Sum(Node root) {
+        if(root == null){
+            return 0;
+        }
+        int letfSum = Sum (root.left);
+        int rightSum = Sum (root.right);
+        return letfSum + rightSum +root.data;
+    }
+    
+}
+public static void main(String[] args) {
+
+
+        //         1
+        //        / \
+        //      2    3
+        //     / \  / \
+        //    4  5 6   7
+
+        Node  root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left =new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7); 
+
+        System.out.print(Node.Sum(root));
+
+}
+} 
