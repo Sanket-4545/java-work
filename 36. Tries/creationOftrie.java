3cbb62f4d46074e848b98279e458e317d7581ebe
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-08-07 22:24:32 +0530

    insertion function

--- 36. Tries/creationOftrie.java
+++ 36. Tries/creationOftrie.java
@@ -1,8 +1,8 @@
 
 public class creationOftrie {
    static class Node{
-        Node Children[] = new Node[28];
-        Boolean eow = false;
+       Node Children[] = new Node[26];
+       boolean eow = false;
 
         Node(){
             for (int i = 0; i< 26 ; i++){
@@ -11,9 +11,27 @@ public class creationOftrie {
 
         }
         public static Node root = new Node();
+
+        public static void insert(String word) {
+            Node curr = root;
+            for (int level = 0 ; level < word.length();level ++){
+                int idx = word.charAt(level) - 'a';
+                if(curr.Children[idx] == null){
+                    curr.Children[idx] = new Node();
+                }
+                curr = curr.Children[idx];
+            }
+
+            curr.eow = true;  
+        }
+      
     }
-public static void main(String[] args) {
+    public static void main(String[] args) {
     String words[] = { "the", "a", "there","their","any","any","three"};
+    for(int i = 0 ; i< words.length;i++){
+        Node.insert(words[i]);
+    }
 }
     
+
 }
