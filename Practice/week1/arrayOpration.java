4ac2451b1f96d4b9ca892638603b3b36fdad3010
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-08-30 17:45:07 +0530

    Linear search

--- Practice/week1/arrayOpration.java
+++ Practice/week1/arrayOpration.java
@@ -1,22 +1,29 @@
 import java.util.*;
 public class arrayOpration {
-
-    public static void reverseArray(int arr[]) {
-        int start = 0;
-        int end = arr.length - 1;
-        while (start < end) {
-            int temp = arr[start];
-            arr[start] = arr[end];
-            arr[end] = temp;
-            start++;
-            end--;
-        }
+// --1--
+    // public static void reverseArray(int arr[]) {
+    //     int start = 0;
+    //     int end = arr.length - 1;
+    //     while (start < end) {
+    //         int temp = arr[start];
+    //         arr[start] = arr[end];
+    //         arr[end] = temp;
+    //         start++;
+    //         end--;
+    //     }
+    // }
+    public static void LinearSearch(int arr[],int key){
+            for(int i = 0 ; i<arr.length; i++){
+                if (key == arr[i]) {
+                System.out.println("Key is found at index:"+arr[i]);
+                }
+            }
     }
-
     public static void main(String[] args) {
         int arr[] = {1, 2, 3, 4, 5};
 
-        reverseArray(arr);
-        System.out.println(Arrays.toString(arr));
+        // reverseArray(arr);
+        // System.out.println(Arrays.toString(arr));
+        LinearSearch(arr, 3);
     }
 }
