f9aff6e4fc76400181fd068a347429cd47d22f3b
Sanket Bhojane <sanketdbhojane045@gmail.com>
2026-08-30 17:15:54 +0530

    introduction of Array

---
+++ Practice/week1/arrayOpration.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public static int reverseArray(int arr[]){
+    int start = 0;
+    int end = arr.length - 1;
+}
+public class arrayOpration {
+
+    int arr[] = {1, 2, 3, 4, 5};
+}
---
+++ Practice/week1/introOfArray.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class arrayOpration {
+
+    public static int sum(int arr[]) {
+        int total = 0;
+        for (int i = 0; i < arr.length; i++) {
+            total = total + arr[i];
+        }
+        return total;
+    }
+    public static int maxElement(int arr[]){
+        int max = 0; 
+        for(int i = 0 ; i<arr.length ; i++){
+            if (arr[i]>max) {
+               max = arr[i]; 
+            }
+        
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {3, 4, 9, 6};
+
+        // Print elements of array
+        System.out.println("Elements of array are:");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+
+        // Sum of elements of array
+        int result = sum(arr);
+        System.out.println("Sum of array elements: " + result);
+
+        int Max = maxElement(arr);
+        System.out.println("Max element of array is: " + Max);
+    }
+}
